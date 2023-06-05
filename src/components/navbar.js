01362a0e81eb2ebb784e4b6b4d2a0c00f685c09f
vangelif <ev.fotiades@yahoo.com>
2023-06-05 23:03:19 +0300

    build navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+const Navbar = () => {
+  const [dropdown, setDropdown] = useState(false);
+  console.log(useState(false));
+
+  useEffect(() => {
+    document.title = `Current state value: ${dropdown}`;
+  }, [dropdown]);
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>
+          <button onClick={() => setDropdown(!dropdown)}>
+            Services <span>&#8595;</span>
+          </button>
+          {dropdown && (
+            <ul>
+              <li>Design</li>
+              <li>Development</li>
+            </ul>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
+
