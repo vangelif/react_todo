0b69585204d0d673985733f26f38fae754c15567
vangelif <ev.fotiades@yahoo.com>
2023-06-05 17:29:58 +0300

    build dummy input

--- src/components/input.js
+++ src/components/input.js
@@ -1,12 +1,18 @@
-return (
-  <>
-    <form onSubmit={handleSubmit} className="form-container">
-      <input
-        // ...
-        className="input-text"
-      />
-      <button className="input-submit">Submit</button>
-    </form>
-    <span className="submit-warning">{message}</span>
-  </>
-);
+const Input = () => {
+  return <div>input field here...</div>;
+};
+export default Input;
+
+// return (
+//   <>
+//     <form onSubmit={handleSubmit} className="form-container">
+//       <input
+//         // ...
+//         className="input-text"
+//       />
+//       <button className="input-submit">Submit</button>
+//     </form>
+//     <span className="submit-warning">{message}</span>
+//   </>
+// );
+
